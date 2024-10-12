933f095d05968323dca4f50ec216f1462403a2a4
HlaingHtet-archon <hlainghtet29122004@gmail.com>
2024-10-12 20:27:32 +0630

    feat: dapper read, create, edit, update, delete

--- HHADotNetCore.ConsoleApp/Program.cs
+++ HHADotNetCore.ConsoleApp/Program.cs
@@ -6,11 +6,20 @@ using System.Data.SqlClient;
 Console.WriteLine("Hello, World!");
 //Console.ReadLine();
 
-AdoDotNetExample adoDotNetExample = new AdoDotNetExample();
+//AdoDotNetExample adoDotNetExample = new AdoDotNetExample();
 //adoDotNetExample.Read();
 //adoDotNetExample.Create();
 //adoDotNetExample.Edit();
 //adoDotNetExample.Update();
-adoDotNetExample.Delete();
+//adoDotNetExample.Delete();
+
+DapperExample dapperExample = new DapperExample();
+//dapperExample.Read();
+//dapperExample.Create("a", "b", "c");
+dapperExample.Edit(6);
+dapperExample.Edit(1);
+//dapperExample.Create("aaa", "bbb", "ccc");
+//dapperExample.Delete(6);
+
 
 Console.ReadKey();
