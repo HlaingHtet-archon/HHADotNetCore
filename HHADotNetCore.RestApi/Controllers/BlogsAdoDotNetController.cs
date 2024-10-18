b0e4022d0c8d39cc5c28591025a3906a253e8e73
HlaingHtet-archon <hlainghtet29122004@gmail.com>
2024-10-18 19:29:26 +0630

    feat: api using ado.net Get & Patch

---
+++ HHADotNetCore.RestApi/Controllers/BlogsAdoDotNetController.cs
@@ -0,0 +1,138 @@
+﻿using HHADotNetCore.Database.Models;
+using HHADotNetCore.RestApi.ViewModels;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
+
+namespace HHADotNetCore.RestApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BlogsAdoDotNetController : ControllerBase
+    {
+        private readonly string _connectionString = "Data Source=.;Initial Catalog=HHADotNetCore;User ID=sa;Password=sasa@123;TrustServerCertificate=True;";
+
+        [HttpGet]
+        public IActionResult GetBlogs()
+        {
+            List<BlogViewModel> lst = new List<BlogViewModel>();
+
+            SqlConnection connection = new SqlConnection(_connectionString);
+            connection.Open();
+
+            string query = @"SELECT [BlogId]
+      ,[BlogTitle]
+      ,[BlogAuthor]
+      ,[BlogContent]
+      ,[DeleteFlag]
+  FROM [dbo].[Tbl_Blog] where deleteFlag = 0";
+            SqlCommand cmd = new SqlCommand(query, connection);
+            SqlDataReader reader = cmd.ExecuteReader();
+            while (reader.Read())
+            {
+                Console.WriteLine(reader["BlogId"]);
+                Console.WriteLine(reader["BlogTitle"]);
+                Console.WriteLine(reader["BlogAuthor"]);
+                Console.WriteLine(reader["BlogContent"]);
+                //lst.Add(new BlogViewModel
+                //{
+                //    Id = Convert.ToInt32(reader["BlogId"]),
+                //    Title = Convert.ToString(reader["BlogTitle"]),
+                //    Author = Convert.ToString(reader["BlogAuthor"]),
+                //    Content = Convert.ToString(reader["BlogContent"]),
+                //    DeleteFlag = Convert.ToBoolean(reader["DeleteFlag"]),
+                //});
+                var item = new BlogViewModel
+                {
+                    Id = Convert.ToInt32(reader["BlogId"]),
+                    Title = Convert.ToString(reader["BlogTitle"]),
+                    Author = Convert.ToString(reader["BlogAuthor"]),
+                    Content = Convert.ToString(reader["BlogContent"]),
+                    DeleteFlag = Convert.ToBoolean(reader["DeleteFlag"]),
+                };
+                lst.Add(item);
+            }
+
+            connection.Close();
+
+            return Ok(lst);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetBlog(int id)
+        {
+            
+        }
+
+        [HttpPost]
+        public IActionResult CreateBlog(TblBlog blog)
+        {
+            
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult UpdateBlog(int id, TblBlog blog)
+        {
+            
+        }
+
+        [HttpPatch("{id}")]
+        public IActionResult PatchBlog(int id, BlogViewModel blog)
+        {
+            string condition = "";
+            if(string.IsNullOrEmpty( blog.Title))
+            {
+                condition += "[BlogTitle] = @BlogTitle, ";
+            }
+            if (string.IsNullOrEmpty(blog.Author))
+            {
+                condition += "[BlogAuthor] = @BlogAuthor, ";
+            }
+            if (string.IsNullOrEmpty(blog.Content))
+            {
+                condition += "[BlogContent] = @BlogContent, ";
+            }
+
+            if(condition.Length == 0)
+            {
+                return BadRequest("Invalid Parameter!");
+            }
+
+            condition = condition.Substring(0, condition.Length - 2);
+
+            SqlConnection connection = new SqlConnection(_connectionString);
+            connection.Open();
+
+            string query = $@"UPDATE [dbo].[Tbl_Blog] SET {condition} WHERE BlogId = @BlogId";
+
+
+            SqlCommand cmd = new SqlCommand(query, connection);
+            cmd.Parameters.AddWithValue("@BlogId", id);
+            if (string.IsNullOrEmpty(blog.Title))
+            {
+                cmd.Parameters.AddWithValue("@BlogTitle", blog.Title);
+            }
+            if (string.IsNullOrEmpty(blog.Author))
+            {
+                cmd.Parameters.AddWithValue("@BlogAuthor", blog.Author);
+            }
+            if (string.IsNullOrEmpty(blog.Content))
+            {
+                cmd.Parameters.AddWithValue("@BlogContent", blog.Content);
+            }
+
+            int result = cmd.ExecuteNonQuery();
+
+            connection.Close();
+
+            return Ok(result > 0 ? "Updating Successful." : "Updating Failed.");
+        }
+
+        [HttpDelete("{int}")]
+        public IActionResult DeleteBlog(int id)
+        {
+            
+        }
+    }
+}
