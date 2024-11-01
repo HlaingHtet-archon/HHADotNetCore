d08f32aee77c4f879e2e870824fd016ed76f218a
HlaingHtet-archon <hlainghtet29122004@gmail.com>
2024-11-01 20:10:12 +0630

    feat: AdoDotNetService and Dapper Service

--- HHADotNetCore.Shared/AdoDotNetService.cs
+++ HHADotNetCore.Shared/AdoDotNetService.cs
@@ -12,16 +12,20 @@ namespace HHADotNetCore.Shared
             _connectionString = connectionString;
         }
 
-        public DataTable Query(string query, SqlParameterModel[] sqlParameters)
+        public DataTable Query(string query, params SqlParameterModel[] sqlParameters)
         {
             SqlConnection connection = new SqlConnection(_connectionString);
             connection.Open();
 
             SqlCommand cmd = new SqlCommand(query , connection);
-            foreach (var sqlParameter in sqlParameters)
+            if(sqlParameters != null)
             {
-                cmd.Parameters.AddWithValue(sqlParameter.Name, sqlParameter.Value);
+                foreach (var sqlParameter in sqlParameters)
+                {
+                    cmd.Parameters.AddWithValue(sqlParameter.Name, sqlParameter.Value);
+                }
             }
+            
             SqlDataAdapter adapter = new SqlDataAdapter(cmd);
             DataTable dt = new DataTable();
             adapter.Fill(dt);
@@ -30,11 +34,39 @@ namespace HHADotNetCore.Shared
 
             return dt;
         }
+
+        public int Execute(string query, params SqlParameterModel[] sqlParameters)
+        {
+            SqlConnection connection = new SqlConnection(_connectionString);
+            connection.Open();
+
+            SqlCommand cmd = new SqlCommand(query, connection);
+            if (sqlParameters != null)
+            {
+                foreach (var sqlParameter in sqlParameters)
+                {
+                    cmd.Parameters.AddWithValue(sqlParameter.Name, sqlParameter.Value);
+                }
+            }
+
+            var result = cmd.ExecuteNonQuery();
+
+            connection.Close();
+
+            return result;
+        }
     }
 
     public class SqlParameterModel
     {
         public string Name { get; set; }
         public object Value { get; set; }
+
+        public SqlParameterModel() { }
+        public SqlParameterModel(string name, object value)
+        {
+            Name = name;
+            Value = value;
+        }
     }
 }
