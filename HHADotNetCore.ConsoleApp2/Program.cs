3c2e96084295fe62ce683cef78a539ace6d4ec35
HlaingHtet-archon <hlainghtet29122004@gmail.com>
2024-11-08 17:26:27 +0630

    feat: Json convert

--- HHADotNetCore.ConsoleApp2/Program.cs
+++ HHADotNetCore.ConsoleApp2/Program.cs
@@ -1,7 +1,43 @@
-﻿// See https://aka.ms/new-console-template for more information
-using HHADotNetCore.Database.Models;
+﻿using HHADotNetCore.Database.Models;
+using Newtonsoft.Json;
 
 Console.WriteLine("Hello, World!");
 
-AppDbContext db = new AppDbContext();
-var lst = db.TblBlogs.ToList();
+//AppDbContext db = new AppDbContext();
+//var lst = db.TblBlogs.ToList();
+
+var blog = new BlogModel
+{
+    Id = 1,
+    Title = "Test Title",
+    Author = "Test Author",
+    Content = "Test Content",
+};
+
+string jsonStr = blog.toJson();
+
+Console.WriteLine(jsonStr);
+
+string jsonStr2 = """{"Id":1,"Title":"Test Title","Author":"Test Author","Content":"Test Content"}""";
+var blog2 = JsonConvert.DeserializeObject<BlogModel>(jsonStr2);
+
+Console.WriteLine(blog2.Title);
+
+Console.ReadLine();
+
+public class BlogModel
+{
+    public int Id { get; set; }
+    public string Title { get; set; }
+    public string Author { get; set; }
+    public string Content { get; set; }
+}
+
+public static class Extensions //DevCode
+{
+    public static string toJson(this object obj)
+    {
+        string jsonStr = JsonConvert.SerializeObject(obj); // C# to Json
+        return jsonStr;
+    }
+}
