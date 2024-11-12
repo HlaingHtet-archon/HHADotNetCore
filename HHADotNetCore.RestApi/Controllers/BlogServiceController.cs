88760b3f9a4b862693295d85410794433acbdc3a
HlaingHtet-archon <hlainghtet29122004@gmail.com>
2024-11-12 16:28:58 +0630

    feat: BlogServiceController

---
+++ HHADotNetCore.RestApi/Controllers/BlogServiceController.cs
@@ -0,0 +1,81 @@
+﻿using HHADotNetCore.Database.Models;
+using HHADotNetCore.Domain.Features.Blog;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace HHADotNetCore.RestApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BlogServiceController : ControllerBase
+    {
+        private readonly BlogService _service;
+
+        public BlogServiceController() 
+        {
+            _service = new BlogService();
+        }
+
+        [HttpGet]
+        public IActionResult GetBlogs()
+        {
+            var lst = _service.GetBlogs();
+            return Ok(lst);
+        }
+        
+        [HttpGet("{id}")]
+        public IActionResult GetBlog(int id)
+        {
+            var item = _service.GetBlog(id);
+            if (item == null)
+            {
+                return NotFound();
+            }
+            return Ok(item);
+        }
+
+        [HttpPost]
+        public IActionResult CreateBlog(TblBlog blog)
+        {
+            var model = _service.CreateBlogs(blog);
+            return Ok(model);
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult UpdateBlog(int id, TblBlog blog)
+        {
+            var item = _service.UpdateBlog(id, blog);
+            if (item == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(item);
+        }
+
+        [HttpPatch("{id}")]
+        public IActionResult PatchBlog(int id, TblBlog blog)
+        {
+            var item = _service.PatchBlog(id, blog);
+            if (item == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(item);
+        }
+
+        [HttpDelete("{int}")]
+        public IActionResult DeleteBlog(int id)
+        {
+            var item = _service.DeleteBlog(id);
+            if (item == null)
+            {
+                return NotFound();
+            }
+
+            return Ok();
+        }
+    }
+}
