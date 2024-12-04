06ae12077bfacb8c73f01ffd615d2fce3003b5ad
HlaingHtet-archon <hlainghtet29122004@gmail.com>
2024-12-04 16:19:16 +0630

    feat: HttpClientExample & RestClientExample

---
+++ HHADotNetCore.ConsoleApp3/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+using HHADotNetCore.ConsoleApp3;
+
+Console.WriteLine("Hello, World!");
+
+HttpClientExample httpClientExample = new HttpClientExample();
+//await httpClientExample.Read();
+//await httpClientExample.Edit(1);
+//await httpClientExample.Edit(101);
+
+//await httpClientExample.Create("test title", "test body", 1);
+await httpClientExample.Update(1, "test title", "test body", 10);
---
+++ HHADotNetCore.ConsoleApp3/RestClientExample.cs
@@ -0,0 +1,107 @@
+﻿using Newtonsoft.Json;
+using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace HHADotNetCore.ConsoleApp3
+{
+    public class RestClientExample
+    {
+        private readonly RestClient _client;
+        private readonly string _postEndPoint = "https://jsonplaceholder.typicode.com/posts";
+
+        public RestClientExample()
+        {
+            _client = new RestClient();
+        }
+
+        public async Task Read()
+        {
+            RestRequest request = new RestRequest(_postEndPoint, Method.Get);
+            var response = await _client.ExecuteAsync(request);
+            if (response.IsSuccessStatusCode)
+            {
+                string jsonStr = response.Content!;
+                Console.WriteLine(jsonStr);
+            }
+        }
+
+        public async Task Edit(int id)
+        {
+            RestRequest request = new RestRequest($"{_postEndPoint}/{id}", Method.Get);
+            var response = await _client.ExecuteAsync(request);
+            if (response.StatusCode == System.Net.HttpStatusCode.NotFound)
+            {
+                Console.WriteLine("No data found.");
+                return;
+            }
+
+            if (response.IsSuccessStatusCode)
+            {
+                string jsonStr = response.Content!;
+                Console.WriteLine(jsonStr);
+            }
+        }
+
+        public async Task Create(string title, string body, int userId)
+        {
+            PostModel requestModel = new PostModel()
+            {
+                body = body,
+                title = title,
+                userId = userId
+            };
+
+            RestRequest request = new RestRequest(_postEndPoint, Method.Post);
+            request.AddJsonBody(requestModel);
+
+            var response = await _client.ExecuteAsync(request);
+            if (response.IsSuccessStatusCode)
+            {
+                Console.WriteLine(response.Content!);
+            }
+        }
+
+        public async Task Update(int id, string title, string body, int userId)
+        {
+            PostModel requestModel = new PostModel()
+            {
+                id = id,
+                body = body,
+                title = title,
+                userId = userId
+            };
+
+            RestRequest request = new RestRequest(_postEndPoint, Method.Patch);
+            request.AddJsonBody(requestModel);
+
+            var response = await _client.ExecuteAsync(request);
+            if (response.IsSuccessStatusCode)
+            {
+                Console.WriteLine(response.Content!);
+            }
+        }
+
+        public async Task Delete(int id)
+        {
+            RestRequest request = new RestRequest($"{_postEndPoint}/{id}", Method.Delete);
+
+            var response = await _client.ExecuteAsync(request);
+            if (response.StatusCode == System.Net.HttpStatusCode.NotFound)
+            {
+                Console.WriteLine("No data found.");
+                return;
+            }
+
+            if (response.IsSuccessStatusCode)
+            {
+                string jsonStr = response.Content!;
+                Console.WriteLine(jsonStr);
+            }
+        }
+    }
+}
