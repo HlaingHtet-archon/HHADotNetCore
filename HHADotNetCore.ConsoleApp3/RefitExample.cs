2680a3acd90938284f1aaae51983a84f44263a5b
HlaingHtet-archon <hlainghtet29122004@gmail.com>
2024-12-06 21:40:29 +0630

    feat: Refit CRUD

---
+++ HHADotNetCore.ConsoleApp3/RefitExample.cs
@@ -0,0 +1,58 @@
+﻿using Refit;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HHADotNetCore.ConsoleApp3
+{
+    public class RefitExample
+    {
+        public async Task Run()
+        {
+            var blogApi = RestService.For<IBlogApi>("http://localhost:5241");
+            var lst = await blogApi.GetBlogs();
+            foreach (var item in lst)
+            {
+                Console.WriteLine(item.BlogTitle);
+            }
+
+            var item2 = await blogApi.GetBlog(1);
+            try
+            {
+                var item3 = await blogApi.GetBlog(100);
+            }
+            catch (ApiException ex)
+            {
+                if(ex.StatusCode == System.Net.HttpStatusCode.NotFound)
+                {
+                    Console.WriteLine("No data found.");
+                }
+            }
+
+            var item4 = await blogApi.CreateBlog(new BlogModel
+            {
+                BlogTitle = "Task",
+                BlogAuthor = "Task",
+                BlogContent = "Task",
+            });
+
+            var item5 = await blogApi.PutBlog(2, new BlogModel
+            {
+                BlogTitle = "Task",
+                BlogAuthor = "Task",
+                BlogContent = "Task",
+            });
+
+            var item6 = await blogApi.PatchBlog(2, new BlogModel
+            {
+                BlogTitle = "Task",
+                BlogAuthor = "Task",
+                BlogContent = "Task",
+            });
+
+            var item7 = await blogApi.DeleteBlog(15);
+        }
+    }
+}
--- HHADotNetCore.MinimalApi/Endpoint/Blog/BlogServiceEndpoint.cs
+++ HHADotNetCore.MinimalApi/Endpoint/Blog/BlogServiceEndpoint.cs
@@ -1,86 +1,86 @@
-﻿using HHADotNetCore.Domain.Features.Blog;
+﻿//using HHADotNetCore.Domain.Features.Blog;
 
-namespace HHADotNetCore.MinimalApi.Endpoint.Blog;
+//namespace HHADotNetCore.MinimalApi.Endpoint.Blog;
 
-public static class BlogServiceEndpoint
-{
-    public static void UseBlogServiceEndpoint(this IEndpointRouteBuilder app)
-    {
-        app.MapGet("/blogs", () =>
-        {
-            BlogService service = new BlogService();
-            var lst = service.GetBlogs();
-            return Results.Ok(lst);
-        })
-.WithName("GetBlogs")
-.WithOpenApi();
+//public static class BlogServiceEndpoint
+//{
+//    public static void UseBlogServiceEndpoint(this IEndpointRouteBuilder app)
+//    {
+//        app.MapGet("/blogs", () =>
+//        {
+//            BlogService service = new BlogService();
+//            var lst = service.GetBlogs();
+//            return Results.Ok(lst);
+//        })
+//.WithName("GetBlogs")
+//.WithOpenApi();
 
-        app.MapGet("/blogs/{id}", (int id) =>
-        {
-            BlogService service = new BlogService();
-            var lst = service.GetBlog();
-            if (lst == null)
-            {
-                return Results.BadRequest("No data found.");
-            }
+//        app.MapGet("/blogs/{id}", (int id) =>
+//        {
+//            BlogService service = new BlogService();
+//            var lst = service.GetBlog();
+//            if (lst == null)
+//            {
+//                return Results.BadRequest("No data found.");
+//            }
 
-            return Results.Ok(lst);
-        })
-        .WithName("GetBlog")
-        .WithOpenApi();
+//            return Results.Ok(lst);
+//        })
+//        .WithName("GetBlog")
+//        .WithOpenApi();
 
-        app.MapPost("/blogs", (TblBlog blog) =>
-        {
-            BlogService service = new BlogService();
-            var model = service.CreateBlog(blog);
-            return Results.Ok(model);
-        })
-        .WithName("CreateBlogs")
-        .WithOpenApi();
+//        app.MapPost("/blogs", (TblBlog blog) =>
+//        {
+//            BlogService service = new BlogService();
+//            var model = service.CreateBlog(blog);
+//            return Results.Ok(model);
+//        })
+//        .WithName("CreateBlogs")
+//        .WithOpenApi();
 
-        _ = app.MapPut("/blogs/{id}", (int id, TblBlog blog) =>
-        {
-            BlogService service = new BlogService();
-            var item = service.UpdateBlog(id, blog);
+//        _ = app.MapPut("/blogs/{id}", (int id, TblBlog blog) =>
+//        {
+//            BlogService service = new BlogService();
+//            var item = service.UpdateBlog(id, blog);
 
-            if (item is null)
-            {
-                return Results.BadRequest("No data found.");
-            }
+//            if (item is null)
+//            {
+//                return Results.BadRequest("No data found.");
+//            }
 
-            return Results.Ok(blog);
-        })
-        .WithName("UpdateBlogs")
-        .WithOpenApi();
+//            return Results.Ok(blog);
+//        })
+//        .WithName("UpdateBlogs")
+//        .WithOpenApi();
 
-        app.MapPatch("/blogs/{id}", (int id, TblBlog blog) =>
-        {
-            BlogService service = new BlogService();
-            var item = service.PatchBlog(id, blog);
+//        app.MapPatch("/blogs/{id}", (int id, TblBlog blog) =>
+//        {
+//            BlogService service = new BlogService();
+//            var item = service.PatchBlog(id, blog);
 
-            if (item is null)
-            {
-                return Results.BadRequest("No data found.");
-            }
+//            if (item is null)
+//            {
+//                return Results.BadRequest("No data found.");
+//            }
 
-            return Results.Ok(blog);
-        })
-        .WithName("UpdateBlog")
-        .WithOpenApi();
+//            return Results.Ok(blog);
+//        })
+//        .WithName("UpdateBlog")
+//        .WithOpenApi();
 
-        app.MapDelete("/blogs/{id}", (int id) =>
-        {
-            BlogService service = new BlogService();
-            var item = service.DeleteBlog(id);
+//        app.MapDelete("/blogs/{id}", (int id) =>
+//        {
+//            BlogService service = new BlogService();
+//            var item = service.DeleteBlog(id);
 
-            if (item is null)
-            {
-                return Results.BadRequest("No data found.");
-            }
+//            if (item is null)
+//            {
+//                return Results.BadRequest("No data found.");
+//            }
 
-            return Results.Ok();
-        })
-        .WithName("DeleteBlogs")
-        .WithOpenApi();
-    }
-}
+//            return Results.Ok();
+//        })
+//        .WithName("DeleteBlogs")
+//        .WithOpenApi();
+//    }
+//}
